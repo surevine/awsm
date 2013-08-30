733d48fb1468af84257f02c8c6473580fe2b25a1
Simon White <simon.white@gmail.com>
2013-08-30 15:25:06 +0100

    GPL

--- awsm.php
+++ awsm.php
@@ -1,3 +1,13 @@
+/**
+* Copyright (c) 2013 simonw.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the GNU Public License v2.0
+* which accompanies this distribution, and is available at
+* http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+* 
+* Contributors:
+*     simonw - initial API and implementation
+**/
 <?php namespace awsm;
 
 
