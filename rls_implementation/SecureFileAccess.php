497fff122c94fd62c32fff0de927402e0a4a6f42
simonw2 <simon.white@gmail.com>
2013-11-06 11:17:11 +0100

    File permissions now work for security groups

--- rls_implementation/SecureFileAccess.php
+++ rls_implementation/SecureFileAccess.php
@@ -11,6 +11,12 @@
 *     simonw - initial API and implementation
 **/
 
+function last_index_of($sub_str,$instr) {
+	if(strstr($instr,$sub_str)!="") {
+		return(strlen($instr)-strpos(strrev($instr),$sub_str));
+	}
+	return(-1);
+}
 
 //Bootstrap mediawiki
 $oldDir=getcwd();
@@ -23,16 +29,30 @@ if ( isset( $_SERVER['MW_COMPILED'] ) ) {
 }
 chdir ($oldDir);
 
+//Now we've established our Mediawiki globals, we can import the relevant dependencies
 require_once( "$IP/includes/GlobalFunctions.php" );
+require_once( "$IP/extensions/awsm/security_business_logic/AccessDecisionManager.php" );
 
-
+//Get the file we're actually trying to access
 $path = filter_input(INPUT_GET,"path",FILTER_SANITIZE_STRING);
 wfErrorLog("Evaluating access for a file: " . $path . "\n", '/tmp/awsm.log');
 
-$path="../../../". $path;
+//Get the name of the corresponding wiki page
+$pageTitle=str_replace('_', ' ', "File:".substr( $path, strrpos( $path, '/' )+1 ));
+wfErrorLog("Wiki page name: " . $pageTitle . "\n", '/tmp/awsm.log');
 
-$imageInfo = getimagesize($path);
-header("Content-Type: " . $imageInfo['mime'] );
-header("Content-Length: " . filesize($path));
-$fp = fopen($path, 'rb');
-fpassthru($fp);
+
+if (\awsm\security_business_logic\AccessDecisionManager::canUserSeePage($pageTitle)) {
+	// We can now get the actual file requested, set some headers, and return it
+	$path="../../../". $path;
+	$imageInfo = getimagesize($path);
+	header("Content-Type: " . $imageInfo['mime'] );
+	header("Content-Length: " . filesize($path));
+	$fp = fopen($path, 'rb');
+	fpassthru($fp);
+}
+else {
+	//We don't have access to the underlying file, so throw a 404
+	echo '<h1>Not Found</h1><p>The resource was not found</p>';
+	header("HTTP/1.0 404 Not Found - Archive Empty");
+}
